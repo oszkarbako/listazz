455f99d40d64bbac9f32f54063a619ca787d4086
Oszkar <oszkar.bako97@gmail.com>
2018-11-29 10:52:11 +0200

    listazz nem mukodik

--- app/src/main/java/com/example/oszkar/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/oszkar/recyclerview/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,21 +17,25 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 import dalvik.system.BaseDexClassLoader;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mBlogList;
-    private FirebaseDatabase mFirebaseDatabase;
-    private DatabaseReference mDatabase;
 
-    private TextView mTextMessage; //buttonnal jart
+    private DatabaseReference mDatabase;
+    private String TAG = "Ez";
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -39,13 +44,13 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    //mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                   // mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    //mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -57,34 +62,70 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        //mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        mFirebaseDatabase = FirebaseDatabase.getInstance();
-        mDatabase = mFirebaseDatabase.getReference("sapiadvertiser-8ea00");
-        mDatabase.keepSynced(true);
+
+        mDatabase =FirebaseDatabase.getInstance().getReference("Data");
 
         mBlogList = (RecyclerView) findViewById(R.id.myRecyclerView);
         mBlogList.setHasFixedSize(true);
         mBlogList.setLayoutManager(new LinearLayoutManager(this));
+
+
+        
+
+
+
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        FirebaseRecyclerAdapter<Blog, BlogViewHolder> firebaseRecyclerAdapter =
-                new FirebaseRecyclerAdapter<Blog, BlogViewHolder>(Blog.class, R.layout.blog_row, BlogViewHolder.class, mDatabase) {
-                    @Override
-                    protected void populateViewHolder(BlogViewHolder viewHolder, Blog model, int position) {
-                        viewHolder.setTitle(model.getTitle());
-                        viewHolder.setDescription(model.getDescription());
-                        viewHolder.setImage(getApplicationContext(), model.getImage());
-                    }
-                };
+        Log.d(TAG, "itt");
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.d(TAG, "itt");
+                //Blog post = dataSnapshot.getValue(Blog.class);
+                Log.d(TAG, dataSnapshot.getValue().toString());
+                collectPhoneNumbers((Map<String,Blog>) dataSnapshot.getValue());
+            }
 
-        mBlogList.setAdapter(firebaseRecyclerAdapter);
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
+
+    private void collectPhoneNumbers(Map<String,Blog> value) {
+
+        ArrayList<Blog> blogs = new ArrayList<>();
+
+        //iterate through each user, ignoring their UID
+        for (Map.Entry<String, Blog> entry : value.entrySet()) {
+
+            //Get user map
+            Map singleUser = (Map) entry.getValue();
+            //Get phone field and append to list
+            // phoneNumbers.add((String) singleUser.get("title"));
+            Blog e = new Blog((String) singleUser.get("title"), (String) singleUser.get("description"), (String) singleUser.get("image"));
+            blogs.add(e);
+        }
+        for (int i = 0; i < blogs.size(); i++) {
+            Log.d(TAG, "title: " + blogs.get(i).getTitle());
+
+
+        }
+
+
+    }
+
 }
 
 
